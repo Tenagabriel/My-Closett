d7f302d936097a7b96debd8837c3b80d7fdc7baa
GabbyFox <tenagabriel13@gmail.com>
2025-06-13 00:45:05 +0200

    made the sidebar buttons functional

--- scripts/main.js
+++ scripts/main.js
@@ -33,7 +33,40 @@ const sideBar = document.querySelector('.side-bar')
   sideBar.classList.toggle('is-showing')
  })
 
+const sideBarHome = document.querySelector('.side-bar-home');
+ sideBarHome.addEventListener('click', () => {
+  window.location.href = 'index.html'
+ })
 
+ const sideBarTrend = document.querySelector('.side-bar-trend');
+ sideBarTrend.addEventListener('click', () => {
+  window.location.href = 'trends.html'
+ })
+
+ const sideBarExplore = document.querySelector('.side-bar-explore');
+ const exploreMenu = document.querySelector('.explore-menu')
+ sideBarExplore.addEventListener('click', () => {
+   exploreMenu.classList.toggle('is-reveal')
+ })
+
+const sideBarAboutUs = document.querySelector('.side-bar-about-us');
+const aboutPopup = document.querySelector('.about-popup');
+const popupClose = document.querySelector('.popup-close');
+
+sideBarAboutUs.addEventListener('click', () => {
+  aboutPopup.classList.add('is-visible'); 
+});
+
+popupClose.addEventListener('click', () => {
+  aboutPopup.classList.remove('is-visible'); 
+});
+
+const sideBarContact = document.querySelector('.side-bar-contact')
+const sideBarMenu = document.querySelector('.contact-menu')
+
+sideBarContact.addEventListener('click', () => {
+  sideBarMenu.classList.toggle('is-shown')
+})
 
 let illustration = ''
 
